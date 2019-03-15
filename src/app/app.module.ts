5de46433c3fa17b95b8d882d0f1aab4ff480a9a9
Nital Patel <patelnital36@gmail.com>
2019-03-15 10:58:17 -0400

    Add ngmodule and router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule} from './app-routing.module';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
     BsNavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule.forRoot()
 
   ],
   providers: [],
