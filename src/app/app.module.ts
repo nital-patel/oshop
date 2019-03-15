684a2dadc98a66910e64470ca2e1872f0a8ab978
Nital Patel <patelnital36@gmail.com>
2019-03-15 11:56:15 -0400

    setup home and shopping routerlink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,10 @@ import { HomeComponent } from './home/home.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule} from '@angular/router';
 import {AngularFireModule} from 'angularfire2';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
-import {AngularFireAuthModule} from 'angularfire2/auth';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductsComponent } from './products/products.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -29,10 +23,6 @@ import { LoginComponent } from './login/login.component';
     ProductsComponent,
     CheckOutComponent,
     OrderSuccessComponent,
-    MyOrdersComponent,
-    AdminProductsComponent,
-    AdminOrdersComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -41,13 +31,12 @@ import { LoginComponent } from './login/login.component';
     NgbModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      {path: '', component: ShoppingCartComponent},
-      {path: '', component: ProductsComponent},
-      {path: '', component: CheckOutComponent},
-      {path: '', component: OrderSuccessComponent},
-      {path: '', component: LoginComponent},
-      {path: 'admin/products', component: AdminProductsComponent},
-      {path: 'admin/orders', component: AdminOrdersComponent}
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'check-out', component: CheckOutComponent},
+      {path: 'order-success', component: OrderSuccessComponent},
+      {path: 'order-success', component: OrderSuccessComponent},
+      {path: 'order-success', component: OrderSuccessComponent}
     ])
   ],
   providers: [],
