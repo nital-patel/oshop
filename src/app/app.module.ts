f5875170eb2de7ed2d46825c3476f979d22d2cd4
Nital Patel <patelnital36@gmail.com>
2019-03-27 17:03:52 -0400

    fix it error golbal undefinded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,16 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule} from '@angular/router';
-import {AngularFireModule} from 'angularfire2';
+import {AngularFireModule } from 'angularfire2';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductsComponent } from './products/products.component';
-import { CheckOutComponent} from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import {CheckOutComponent} from './check-out/check-out.component';
+import {OrderSuccessComponent} from './order-success/order-success.component';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,6 @@ import { LoginComponent } from './login/login.component';
     ProductsComponent,
     CheckOutComponent,
     OrderSuccessComponent,
-    MyOrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
@@ -35,6 +35,8 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase),
     NgbModule,
     RouterModule.forRoot([
@@ -42,7 +44,6 @@ import { LoginComponent } from './login/login.component';
       {path: 'shopping-cart', component: ShoppingCartComponent},
       {path: 'products', component: ProductsComponent},
       {path: 'check-out', component: CheckOutComponent},
-      {path: 'my/orders', component: MyOrdersComponent},
       {path: 'order-success', component: OrderSuccessComponent},
       {path: 'login', component: LoginComponent},
       {path: 'admin/products', component: AdminProductsComponent},
